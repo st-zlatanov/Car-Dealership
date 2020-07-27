5fc7fabceca117b2056db1da6fb6eeff6cdb8058
st-zlatanov <stiliyan.zlatanov@gmail.com>
2020-07-27 18:22:19 +0300

    remove unused imports

--- finalProjectForDefense/src/main/java/com/softuni/service/impl/AuthenticatedUserServiceImpl.java
+++ finalProjectForDefense/src/main/java/com/softuni/service/impl/AuthenticatedUserServiceImpl.java
@@ -2,11 +2,9 @@ package com.softuni.service.impl;
 
 import com.softuni.service.AuthenticatedUserService;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
-import java.util.Collection;
 
 @Component
 
